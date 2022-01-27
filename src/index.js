cec74081de752b87452f50293bd07c46163918bb
Slava-Shchipunov <shchipunovsd@gmail.com>
2022-01-28 05:12:44 +0700

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+        // получаем массив строк, длинной 10 символов каждая
+        let symbolsArray = expr.match(/.{1,10}/g);
+
+        // создаём необходимые переменные
+        let dubleArray = [];
+        let resultStr = '';
+        let morseStr;
+
+        // функция перевода строки в код Морзе
+        const morse = (str) => {
+            dubleArray = str.match(/.{1,2}/g);
+            morseStr = '';
+            for (let i = 0; i < dubleArray.length; i++) {
+                if (dubleArray[i] === '10') {
+                    morseStr = morseStr + '.';
+                } else if (dubleArray[i] === '11') {
+                    morseStr = morseStr + '-';
+                } else if (dubleArray[i] === '00') {
+                    morseStr = morseStr + '';
+                }
+            }
+        }
+
+        // декодируем символы
+        for (let j = 0; j < symbolsArray.length; j++) {
+            if (symbolsArray[j] === '**********') {
+                resultStr = resultStr + ' ';
+            } else {
+                morse(symbolsArray[j]);
+                resultStr = resultStr + MORSE_TABLE[morseStr];
+            }            
+        } 
+        
+        return resultStr;
 }
 
 module.exports = {
